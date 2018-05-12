cdebf0403ad1b6abe296a0da0c85c24c4ecd248d
Anh Ho <anhhothai@gmail.com>
2018-05-12 23:40:10 +0700

    Refine main.js & add references.json to load useful referencing urls

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,23 +1,26 @@
-function create_links(arr_list){
-    var elGrid4 = document.getElementById(arr_list.elementId);
-    var items = arr_list.items;
-    var itemCount = items.length;
-    for(var i = 0; i < itemCount; i++){
-        // Create new elements li tag
-        var newLi = document.createElement("li");
-        elGrid4.appendChild(newLi);
-        // Create new textNode: heading title
-        var newText = document.createTextNode(items[i].text);
-        newLi.appendChild(newText);
-        // Create a new a tag
-        var newA = document.createElement("a");
-        newA.style.marginLeft = "10px";
-        newA.setAttribute('target', 'blank');
-        newA.setAttribute('href', items[i].url);
-        // Create a textNode: [read more..]
-        var newReadMore = document.createTextNode("[read more..]");
-        newA.appendChild(newReadMore);
-        newLi.appendChild(newA);
+function create_links(aData){
+    var aGroups = aData.items;
+    var groupCount = aGroups.length;
+    for(var i = 0; i < groupCount; i++){
+        var elementUl = document.getElementById(aGroups[i].id);
+        var items = aGroups[i].items;
+        var itemCount = items.length;
+        for(var j = 0; j < itemCount; j++){
+            // Create new elements li tag
+            var newLi = document.createElement("li");
+            elementUl.appendChild(newLi);
+            // Create new textNode: heading title
+            var newText = document.createTextNode(items[j].text);
+            newLi.appendChild(newText);
+            // Create a new a tag
+            var newA = document.createElement("a");
+            newA.style.marginLeft = "10px";
+            newA.setAttribute('target', 'blank');
+            newA.setAttribute('href', items[j].url);
+            // Create a textNode: [read more..]
+            var newReadMore = document.createTextNode("[read more..]");
+            newA.appendChild(newReadMore);
+            newLi.appendChild(newA);
+        }
     }
-
 }
