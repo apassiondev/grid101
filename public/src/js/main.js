2daccf66ccd1901f3afc5ef888062413ed887ef4
Anh Ho <anhhothai@gmail.com>
2018-05-12 19:55:59 +0700

    Tips to name your Grid layout for better item positioning.

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,33 +1,23 @@
-function create_links(elementId){
-    var arr_list = [
-        {
-            "text": "Understand the CSS Auto-Placement Algorithm from Envato Tuts+",
-            "url": "https://webdesign.tutsplus.com/tutorials/understanding-the-css-grid-auto-placement-algorithm--cms-27563"
-        },
-        {
-            "text": "Auto-Placement in CSS Grid Layout - MDN",
-            "url": "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Auto-placement_in_CSS_Grid_Layout"
-        }
-    ];
-
-    var elGrid4 = document.getElementById(elementId);
-    for(var i = 0; i < arr_list.length; i++){
-        // Create new elements li
+function create_links(arr_list){
+    var elGrid4 = document.getElementById(arr_list.elementId);
+    var items = arr_list.items;
+    var itemCount = items.length;
+    for(var i = 0; i < itemCount; i++){
+        // Create new elements li tag
         var newLi = document.createElement("li");
         elGrid4.appendChild(newLi);
-        // Create new elements textNode (heading title)
-        var newText = document.createTextNode(arr_list[i].text);
+        // Create new textNode: heading title
+        var newText = document.createTextNode(items[i].text);
         newLi.appendChild(newText);
-        // Create a new a
+        // Create a new a tag
         var newA = document.createElement("a");
         newA.style.marginLeft = "10px";
         newA.setAttribute('target', 'blank');
-        newA.setAttribute('href', arr_list[i].url);
-        // Create [read more..]
+        newA.setAttribute('href', items[i].url);
+        // Create a textNode: [read more..]
         var newReadMore = document.createTextNode("[read more..]");
         newA.appendChild(newReadMore);
         newLi.appendChild(newA);
-
     }
 
 }
